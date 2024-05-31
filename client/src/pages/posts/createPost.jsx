58180a0e6cc6ca64197ee9197d54329ac083f297
Darsheel Chudal <74804981+darsheelchudal@users.noreply.github.com>
2024-05-31 06:50:30 +0545

    Ui completed

--- client/src/pages/posts/createPost.jsx
+++ client/src/pages/posts/createPost.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Sidebar from "../../components/sidebar";
+import { useState, useEffect } from "react";
 
 function CreatePost() {
   return (
@@ -74,43 +75,44 @@ function CreatePost() {
                     </div>
                   </div>
 
-                  <div className="col-span-full">
+                  <div class="col-span-full">
                     <label
-                      htmlFor="upload-photo"
-                      className="block text-sm font-medium leading-6 text-gray-900"
+                      for="cover-photo"
+                      class="block text-sm font-medium leading-6 text-gray-900"
                     >
-                      Upload photo
+                      Cover photo
                     </label>
-                    <div className="mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10">
-                      <div className="text-center">
+                    <div class="mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10">
+                      <div class="text-center">
                         <svg
-                          className="mx-auto h-12 w-12 text-gray-300"
+                          class="mx-auto h-12 w-12 text-gray-300"
                           viewBox="0 0 24 24"
                           fill="currentColor"
                           aria-hidden="true"
                         >
                           <path
-                            fillRule="evenodd"
+                            fill-rule="evenodd"
                             d="M1.5 6a2.25 2.25 0 012.25-2.25h16.5A2.25 2.25 0 0122.5 6v12a2.25 2.25 0 01-2.25 2.25H3.75A2.25 2.25 0 011.5 18V6zM3 16.06V18c0 .414.336.75.75.75h16.5A.75.75 0 0021 18v-1.94l-2.69-2.689a1.5 1.5 0 00-2.12 0l-.88.879.97.97a.75.75 0 11-1.06 1.06l-5.16-5.159a1.5 1.5 0 00-2.12 0L3 16.061zm10.125-7.81a1.125 1.125 0 112.25 0 1.125 1.125 0 01-2.25 0z"
-                            clipRule="evenodd"
+                            clip-rule="evenodd"
                           />
                         </svg>
-                        <div className="mt-4 flex text-sm leading-6 text-gray-600">
+                        <div class="mt-4 flex text-sm leading-6 text-gray-600">
                           <label
-                            htmlFor="file-upload"
-                            className="relative cursor-pointer rounded-md bg-white font-semibold text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 hover:text-indigo-500"
+                            for="file-upload"
+                            class="relative cursor-pointer rounded-md bg-white font-semibold text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 hover:text-indigo-500"
                           >
                             <span>Upload a file</span>
                             <input
                               id="file-upload"
                               name="file-upload"
                               type="file"
-                              className="sr-only"
+                              class="sr-only"
+                              accept="image/*"
                             />
                           </label>
-                          <p className="pl-1">or drag and drop</p>
+                          <p class="pl-1">or drag and drop</p>
                         </div>
-                        <p className="text-xs leading-5 text-gray-600">
+                        <p class="text-xs leading-5 text-gray-600">
                           PNG, JPG, GIF up to 10MB
                         </p>
                       </div>
@@ -122,7 +124,7 @@ function CreatePost() {
 
             <div className="mt-6 flex items-center justify-center gap-x-6">
               <button
-                type="submit"
+                type="su    bmit"
                 className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
                 Save
