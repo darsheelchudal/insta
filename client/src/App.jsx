515fe891261ece6cc02c18a48d15033541192402
Darsheel Chudal <74804981+darsheelchudal@users.noreply.github.com>
2024-05-31 06:24:33 +0545

    cerate Form

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Home from "./pages/Home";
 import Profile from "./pages/Profile";
+import CreatePost from "./pages/posts/createPost";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -17,6 +18,7 @@ function App() {
           <Route path="/sign-up" element={<Signup />} />
           <Route path="/home" element={<Home />} />
           <Route path="/profile" element={<Profile />} />
+          <Route path="/create" element={<CreatePost />} />
         </Routes>
       </BrowserRouter>
     </>
